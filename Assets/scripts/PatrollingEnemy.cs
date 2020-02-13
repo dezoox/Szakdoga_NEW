3d14b2874472cb443aa27e4934476686025ec858
dezoox <43348651+dezoox@users.noreply.github.com>
2020-02-13 19:34:02 +0100

    Created patrolling enemy prefab, does not work yet
    Created a new Scene with Player and Patrolling Enemy, implemented the code for the Enemy to wander, but does not work yet, I commented it out. NavMesh applied to ground terrain.

---
+++ Assets/scripts/PatrollingEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrollingEnemy : MonoBehaviour
+{
+    //private float wanderRange;
+    //private float wanderTime;
+
+    //private Transform target;
+    //private NavMeshAgent agent;
+    //private float timer;
+
+    //void OnEnable()
+    //{
+    //    agent = GetComponent<NavMeshAgent>();
+    //    timer = wanderTime;
+    //}
+    //void Update()
+    //{
+    //    timer += Time.deltaTime;
+    //    if (timer >= wanderTime)
+    //    {
+    //        Vector3 newPosition = WanderingRandomPosition(transform.position, wanderRange, -1);
+    //        agent.SetDestination(newPosition);
+    //        timer = 0;
+    //    }
+    //}
+
+    //private Vector3 WanderingRandomPosition(Vector3 origin, float distance, int layermask)
+    //{
+    //    Vector3 RandomDirection = Random.insideUnitSphere * distance;
+    //    RandomDirection += origin;
+    //    NavMeshHit navHit;
+    //    NavMesh.SamplePosition(RandomDirection, out navHit, distance, layermask);
+    //    return navHit.position;
+    //}
+}
