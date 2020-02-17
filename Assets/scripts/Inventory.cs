4f9dd48a1d24a2633d0ba827f3ff7c032e8256e2
dezoox <43348651+dezoox@users.noreply.github.com>
2020-02-17 15:58:14 +0100

    created basic item class with properties, and itemdatabase class
    Also created 2 gameobject, which are "ItemDatabase" (and also added a tag "ItemDatabase" for it), and "Inventory".

---
+++ Assets/scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> inventory = new List<Item>();
+    public ItemDatabase database;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item 
+{
+    public string itemName;
+    public int itemID;
+    public string itemDescritpion;
+    public Texture2D itemIcon;
+    public int itemPower;
+    public int itemSpeed;
+    public ItemType itemType;
+
+    public enum ItemType
+    {
+        Weapon,
+        Armor,
+        Consumable,
+        Quest
+    }
+}
