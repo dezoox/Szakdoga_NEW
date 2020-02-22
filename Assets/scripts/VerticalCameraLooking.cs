d118f61ef8e46ef49152b8ea9d5762e1a1ecd90a
dezoox <43348651+dezoox@users.noreply.github.com>
2020-02-22 13:30:25 +0100

    Impelemented camera vertical movement

---
+++ Assets/scripts/VerticalCameraLooking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalCameraLooking : MonoBehaviour
+{
+    private float mouseSpeed = 50f;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float rotationX = -Input.GetAxis("Mouse Y");
+            transform.Rotate(rotationX * mouseSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
